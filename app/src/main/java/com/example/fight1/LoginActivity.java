869f2c7a7fde85b74862fa7986362df58233f682
xinwei zheng <xzhe0030@student.monash.edu>
2022-04-23 17:57:43 +1000

    完成了week3的基本部分 后面的炫酷部分 coki没有写

--- app/src/main/java/com/example/fight1/LoginActivity.java
+++ app/src/main/java/com/example/fight1/LoginActivity.java
@@ -2,14 +2,23 @@ package com.example.fight1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fight1.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -18,10 +27,37 @@ public class LoginActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         binding.signInTextView.setOnClickListener(v ->{
             Intent intent = new Intent(LoginActivity.this, SigninActivity.class);
             startActivity(intent);
         });
+
+
+        binding.LoginButton.setOnClickListener(v->{
+            //在这之前 可以写一个验证 email 和password的方法
+            mAuth.signInWithEmailAndPassword(binding.UserName.getText().toString(), binding.UserPassword.getText().toString())
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            // 写日志的目前不需要Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Authentication successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //写日志的 Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    });
+        });
+
 
     }
 }
--- app/src/main/java/com/example/fight1/SigninActivity.java
+++ app/src/main/java/com/example/fight1/SigninActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fight1.databinding.ActivitySigninBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SigninActivity extends AppCompatActivity {
     private ActivitySigninBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -19,6 +27,8 @@ public class SigninActivity extends AppCompatActivity {
         binding = ActivitySigninBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
 
         binding.backTextView.setOnClickListener(v ->{
             Intent intent = new Intent(SigninActivity.this, LoginActivity.class);
@@ -32,18 +42,66 @@ public class SigninActivity extends AppCompatActivity {
     }
 
     private void validationWhenSignin(){
-        binding.emailInPut.getText();
+        boolean emailIsValid = false;
         //empty
         if(binding.emailInPut.getText().toString().isEmpty()){
+            //getResources().getString(R.string.email_empty_error) 这一串是调用string里面的字符串 来显示
             binding.emailInPutWrong.setError(getResources().getString(R.string.email_empty_error));
+            emailIsValid = false;
         }
         //格式
         else if (!Patterns.EMAIL_ADDRESS.matcher(binding.emailInPut.getText().toString()).matches()){
             binding.emailInPutWrong.setError(getResources().getString(R.string.email_format_error));
+            emailIsValid = false;
         }
         else{
             binding.emailInPutWrong.setErrorEnabled(false);
+            emailIsValid = true;
         }
-        //正确
+        //这里同时加入其他的验证 用&& 相连接
+        if (emailIsValid){
+            binding.signinProgressBar.setVisibility(View.VISIBLE); //把那个转圈的加载小圈 变成可见的 如果不想要可见 可以把 visible改成gone.
+
+            mAuth.createUserWithEmailAndPassword(binding.emailInPut.getText().toString(), binding.passwordInPut.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                //Log.d(TAG, "createUserWithEmail:success");
+                                binding.signinProgressBar.setVisibility(View.GONE);
+                                Toast.makeText(SigninActivity.this, "Sign up successfully!", Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startActivity(new Intent(SigninActivity.this, LoginActivity.class));
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SigninActivity.this, "Sign up failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
+
+
+
+//            new Thread(() -> { //新建一个线程，避免了 ui 和后端 双线程导致显示顺序的错误
+//                try {
+//                    Thread.sleep(5000); //5000是5秒， 系统会在执行完上一条代码后 在这里停留5秒 需要try catch不然会报错
+//                }
+//                catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//                runOnUiThread(() -> {
+//                    //一个pop up 在所有验证成功之后在这个界面弹出一个小气泡显示text内容
+//                    Toast.makeText(SigninActivity.this, "Sign up successfully!", Toast.LENGTH_SHORT).show();
+//                    binding.signinProgressBar.setVisibility(View.GONE);
+//                });
+//            }).start(); //start 开启这个线程
+
+
+
+        }
     }
 }
